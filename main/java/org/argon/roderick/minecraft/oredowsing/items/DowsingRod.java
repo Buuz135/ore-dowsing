e11f6b4dcd470fcdd20b047409ffee00e4ea2cf1
Roderick Schertler <roderick@argon.org>
2015-05-13 08:27:54 -0400

    oops, you have to use NBT to store data which varies per-item

--- main/java/org/argon/roderick/minecraft/oredowsing/items/DowsingRod.java
+++ main/java/org/argon/roderick/minecraft/oredowsing/items/DowsingRod.java
@@ -6,14 +6,12 @@ import org.argon.roderick.minecraft.oredowsing.lib.Reference;
 import org.argon.roderick.minecraft.oredowsing.render.DowsingRodRenderer;
 import org.lwjgl.input.Keyboard;
 
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.ChunkCoordinates;
 import net.minecraft.world.World;
@@ -24,10 +22,12 @@ public class DowsingRod extends Item
 	private static final String BASE_NAME       = "DowsingRod";              
 	private static final int    DAMAGE_PER_USE  = 1;                         
 	private static final float  RENDER_DURATION = 30.0F;                     
+
+	private static final String NBT_RADIUS		= "radius";
+	private static final String NBT_TARGET_BLOCK_ID = "block_id";
                                                                                  
-	private Block forcedTargetBlock, curTargetBlock;
-	private int[] curTargetOreIDs = {};
-	private int baseSquareRadius, curSquareRadius;
+	private Block forcedTargetBlock; // null for any ore
+	private int baseSquareRadius;
 
     public DowsingRod(String parNamePrefix, Block parForcedTargetBlock, int parMaxDamage, int parSquareRadius)
     {
@@ -38,20 +38,51 @@ public class DowsingRod extends Item
 		setMaxDamage(parMaxDamage);
         setCreativeTab(CreativeTabs.tabTools);
         
-        forcedTargetBlock = parForcedTargetBlock; // null for any block
-        if (forcedTargetBlock != null) {
-        	forceSetCurTargetBlock(forcedTargetBlock, null);
-        }
-
-        baseSquareRadius  = curSquareRadius = parSquareRadius;
+        forcedTargetBlock = parForcedTargetBlock;
+        baseSquareRadius  = parSquareRadius;
     }
     
+    private void initNBT(ItemStack stack)
+    {
+    	if (stack.stackTagCompound == null) {
+    		stack.stackTagCompound= new NBTTagCompound();
+    	}
+        if (forcedTargetBlock != null) {
+        	forceSetTargetBlock(stack, forcedTargetBlock, null);
+        }
+    	stack.stackTagCompound.setInteger(NBT_RADIUS, baseSquareRadius);
+    }
+    
+    private Block getTargetBlock(ItemStack stack)
+    {
+    	if (stack.stackTagCompound == null) {
+    		initNBT(stack);
+    	}
+    	int target_block_id = stack.stackTagCompound.getInteger(NBT_TARGET_BLOCK_ID);
+    	return target_block_id == 0 ? null : Block.getBlockById(target_block_id);
+    }
+
+    private int getSquareRadius(ItemStack stack)
+    {
+    	if (stack.stackTagCompound == null) {
+    		initNBT(stack);
+    	}
+    	return stack.stackTagCompound.getInteger(NBT_RADIUS);
+    }
+
     @Override
     public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean par4)
     {
-    	list.add("Right-click to highlight "
-    			+ (curTargetBlock != null ? curTargetBlock.getLocalizedName() : "ores"));
-    	list.add("inside a size " + (1+2*curSquareRadius) + " cube around you.");
+    	if (stack.stackTagCompound == null) {
+    		initNBT(stack);
+    	}
+
+    	Block b = getTargetBlock(stack);
+    	list.add("Right-click to highlight " + (b != null ? b.getLocalizedName() : "ores"));
+    	list.add("inside a size " + (1+2*getSquareRadius(stack)) + " cube around you.");
+    	if (forcedTargetBlock == null) {
+    		list.add("Shift-right-click to change target block.");
+    	}
     }
     
     private boolean isShifted()
@@ -63,7 +94,7 @@ public class DowsingRod extends Item
     public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player)
     {
     	if (isShifted()) {
-    		setCurTargetBlock(null, world.isRemote ? null : player);
+    		setTargetBlock(stack, null, world.isRemote ? null : player);
     	}
     	else {
     		divine(stack, world, player);
@@ -75,41 +106,46 @@ public class DowsingRod extends Item
     public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ)
     {
     	if (isShifted()) {
-    		return setCurTargetBlock(world.getBlock(x, y, z),
+    		setTargetBlock(stack, world.getBlock(x, y, z),
     				world.isRemote ? null : player);
     	}
     	else {
     		divine(stack, world, player);
-    		return true;
     	}
+    	return true;
     }
     
-	public boolean setCurTargetBlock(Block targetBlock, EntityPlayer player)
+	public void setTargetBlock(ItemStack stack, Block targetBlock, EntityPlayer player)
 	{
 		if (forcedTargetBlock != null) {
 			if (player != null) {
 				player.addChatMessage(new ChatComponentText("This item doesn't allow changing the target block"));
 			}
-			return false;
+			return;
 		}
-		forceSetCurTargetBlock(targetBlock, player);
-		return true;
+		forceSetTargetBlock(stack, targetBlock, player);
 	}
 	
-    private void forceSetCurTargetBlock(Block targetBlock, EntityPlayer player)
+    private void forceSetTargetBlock(ItemStack stack, Block targetBlock, EntityPlayer player)
     {
-    	// XXX support nether ores?  e.g. oreIron matching oreNetherIron
-    	curTargetBlock  = targetBlock;
-    	curTargetOreIDs = (targetBlock == null ? new int[0] : OreDictionary.getOreIDs(new ItemStack(targetBlock)));
+    	if (stack.stackTagCompound == null) {
+    		initNBT(stack);
+    	}
+        stack.stackTagCompound.setInteger(NBT_TARGET_BLOCK_ID,
+        		targetBlock == null ? 0 : Block.getIdFromBlock(targetBlock));
     	if (player != null) {
     		player.addChatMessage(new ChatComponentText("Target set to "
     				+ (targetBlock == null ? "all ores" : targetBlock.getLocalizedName())));
     	}
     }
     
-    public boolean blockMatches(Block worldBlock)
+    	// XXX does this have to use stack?
+    public boolean blockMatches(ItemStack stack, Block worldBlock)
     {
+    	Block targetBlock = getTargetBlock(stack);
+
     	// detect specific block, but use ore dictionary
+/*
     	if (curTargetOreIDs.length > 0) {
     		int[] worldOreIDs = OreDictionary.getOreIDs(new ItemStack(worldBlock));
     		for (int targ_id : curTargetOreIDs) {
@@ -121,12 +157,14 @@ public class DowsingRod extends Item
     		}
     		return false;
     	}
+*/
+    	if (false) {}
     	
     	// detect specific block, no ore dictionary
-    	else if (curTargetBlock != null) {
+    	else if (targetBlock != null) {
     		return OreDictionary.itemMatches(
     				new ItemStack(worldBlock),
-    				new ItemStack(curTargetBlock),
+    				new ItemStack(targetBlock),
     				true);
     	}
     	
@@ -147,12 +185,13 @@ public class DowsingRod extends Item
     	if (!world.isRemote)
     		return;
     	
+    	int r = getSquareRadius(stack);
         int x, y, z;
     	int ct = 0;
-    	for (x = (int)player.posX - curSquareRadius; x <= player.posX + curSquareRadius; x++) {
-    		for (y = (int)player.posY - curSquareRadius; y <= player.posY + curSquareRadius; y++) {
-    			for (z = (int)player.posZ - curSquareRadius; z <= player.posZ + curSquareRadius; z++) {
-    				if (blockMatches(world.getBlock(x, y, z))) {
+    	for (x = (int)player.posX - r; x <= player.posX + r; x++) {
+    		for (y = (int)player.posY - r; y <= player.posY + r; y++) {
+    			for (z = (int)player.posZ - r; z <= player.posZ + r; z++) {
+    				if (blockMatches(stack, world.getBlock(x, y, z))) {
     					ct++;
     					DowsingRodRenderer.addBlockToHighlight(new ChunkCoordinates(x, y, z), world, player, RENDER_DURATION);
     				}
@@ -160,5 +199,5 @@ public class DowsingRod extends Item
     		}
     	}
     }
-
+    
 }
