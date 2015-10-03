a46de16e55c062e65208e876d9e611520a3dbc9b
Roderick Schertler <roderick.schertler@ibcinc.com>
2015-10-02 20:22:05 -0400

    Commit version for 1.7.10-0.6 release.

--- src/main/java/org/argon/roderick/minecraft/oredowsing/lib/Reference.java
+++ src/main/java/org/argon/roderick/minecraft/oredowsing/lib/Reference.java
@@ -3,5 +3,5 @@ package org.argon.roderick.minecraft.oredowsing.lib;
 public class Reference {
     public static final String MODID   = "oredowsing";
     public static final String NAME    = "Ore Dowsing";
-    public static final String VERSION = "0.4";
+    public static final String VERSION = "0.6";
 }
