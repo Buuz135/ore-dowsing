e050eab5471807b85be5d851bc43893564d93976
Roderick Schertler <roderick.schertler@ibcinc.com>
2015-10-18 07:22:02 -0400

    set order of config properties, use categories

--- src/main/java/org/argon/roderick/minecraft/oredowsing/init/ModItems.java
+++ src/main/java/org/argon/roderick/minecraft/oredowsing/init/ModItems.java
@@ -1,6 +1,7 @@
 package org.argon.roderick.minecraft.oredowsing.init;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.argon.roderick.minecraft.oredowsing.items.DowsingRod;
 import org.argon.roderick.minecraft.oredowsing.lib.Constants;
@@ -17,7 +18,47 @@ public class ModItems
 {
     public static ArrayList<DowsingRod> dowsingRods = new ArrayList<DowsingRod>();
 
-    public static void createDowsingRod(Configuration config,
+    // configuration helper which puts properties in the order in which they
+    // were called
+
+    private static class OrderedConfigCategory {
+        Configuration config;
+        String cat;
+        List<String> propOrder = new ArrayList<String>();
+
+        public OrderedConfigCategory(Configuration parConfig, String parCategory)
+        {
+            this.config = parConfig;
+            this.cat = parCategory;
+        }
+
+        public void close()
+        {
+            config.setCategoryPropertyOrder(this.cat, this.propOrder);
+        }
+
+        public boolean get(String key, boolean def, String comment)
+        {
+            this.propOrder.add(key);
+            return config.get(cat, key, def, comment).getBoolean();
+        }
+
+        public int get(String key, int def, String comment)
+        {
+            this.propOrder.add(key);
+            return config.get(cat, key, def, comment).getInt();
+        }
+
+        public String get(String key, String def, String comment)
+        {
+            this.propOrder.add(key);
+            return config.get(cat, key, def, comment).getString();
+        }
+
+    }
+
+    public static void createDowsingRod(
+            Configuration parConfig,
             String parNamePrefix,
             String parIngredientBaseName,
             String parIngredientTopName,
@@ -31,26 +72,33 @@ public class ModItems
             int parItemsPerUpgrade,
             int parMaxSquareRadius)
     {
-        String  namePrefix         = parNamePrefix;
-        String  cat                = namePrefix.toLowerCase() + "_dowsing_rod";
+        String namePrefix = parNamePrefix;
+        String cat = "rod." + namePrefix.toLowerCase();
+        OrderedConfigCategory myconf = new OrderedConfigCategory(parConfig, cat);
 
-        ItemStack ingredientBase   = getStackForString(
-            config.get(cat, "ingredient_base", parIngredientBaseName, "main crafting ingredient").getString());
-        ItemStack ingredientTop    = getStackForString(
-            config.get(cat, "ingredient_tip", parIngredientTopName, "crafting ingredient for tip").getString());
-        Block   forcedTargetBlock  = getBlockForString(
-            config.get(cat, "target_block", parForcedTargetBlockName, "block detected, empty for all ores").getString());
-        ItemStack upgradeItem    = parUpgradeItemName == null ? null : getStackForString(
-            config.get(cat, "upgrade_item", parUpgradeItemName, "crafting ingredient used to upgrade radius").getString());
-        boolean allowTargetChange  = config.get(cat, "allow_target_block_change", parAllowTargetChange, "true to allow changing which block is detected").getBoolean();
-        int     maxDamage          = config.get(cat, "num_uses",                  parMaxDamage        , "number of uses").getInt();
-        int     squareRadius       = config.get(cat, "radius_base",               parSquareRadius     , "detection area is 1+2*radius cube").getInt();
-        boolean showOreColor       = config.get(cat, "show_ore_color",            parShowOreColor     , "true to color detected ores by type").getBoolean();
-        boolean isChargeable       = config.get(cat, "is_chargeable",             parIsChargeable     , "true to allow repairing with RF").getBoolean();
-        int     itemsPerUpgrade    = config.get(cat, "upgrade_item_count",        parItemsPerUpgrade  , "number of upgrade items required to increase radius by 1").getInt();
-        int     maxSquareRadius    = config.get(cat, "radius_max",                parMaxSquareRadius  , "maximum upgraded radius, 0 if not upgradable").getInt();
+        //System.out.println("configuring " + cat);
+        parConfig.setCategoryComment(cat, "settings for the " + namePrefix.toLowerCase() + " dowsing rod");
 
-        if (!config.get(cat, "enabled", true, "false to disable this dowsing rod completely").getBoolean()) {
+        boolean enabled            = myconf.get("enabled",                   true                    , "false to disable this dowsing rod completely");
+        ItemStack ingredientBase   =
+                   getStackForString(myconf.get("ingredient_base",           parIngredientBaseName   , "main crafting ingredient"));
+        ItemStack ingredientTop    =
+                   getStackForString(myconf.get("ingredient_tip",            parIngredientTopName    , "crafting ingredient for tip"));
+        Block   forcedTargetBlock  =
+                   getBlockForString(myconf.get("target_block",              parForcedTargetBlockName, "block detected, empty for all ores"));
+        boolean allowTargetChange  = myconf.get("allow_target_block_change", parAllowTargetChange    , "true to allow changing which block is detected");
+        int     maxDamage          = myconf.get("num_uses",                  parMaxDamage            , "number of uses");
+        int     squareRadius       = myconf.get("radius_base",               parSquareRadius         , "detection area is 1+2*radius cube");
+        boolean showOreColor       = myconf.get("show_ore_color",            parShowOreColor         , "true to color detected ores by type");
+        boolean isChargeable       = myconf.get("is_chargeable",             parIsChargeable         , "true to allow repairing with RF");
+        ItemStack upgradeItem      =
+                   getStackForString(myconf.get("upgrade_item",              parUpgradeItemName      , "crafting ingredient used to upgrade radius"));
+        int     itemsPerUpgrade    = myconf.get("upgrade_item_count",        parItemsPerUpgrade      , "number of upgrade items required to increase radius by 1");
+        int     maxSquareRadius    = myconf.get("radius_max",                parMaxSquareRadius      , "maximum upgraded radius, 0 if not upgradeable");
+
+        myconf.close();
+
+        if (!enabled) {
             return;
         }
 
@@ -70,9 +118,9 @@ public class ModItems
         // - read list of rods to create from config (getStringList())
 
         //                        name       base ingredient         tip ingredient                           target                 change |uses|radius|color|charge|        upgrade item|upgrade cost|max radius
-        createDowsingRod(config, "Wood",    "minecraft:stick",      "minecraft:coal",                        "minecraft:iron_ore",    false,   50,     4, true, false,                null,           0,         0);
-        createDowsingRod(config, "Iron",    "minecraft:iron_ingot", "minecraft:redstone",                    "minecraft:gold_ore",    false,  100,     6, true, false,                null,           0,         0);
-        createDowsingRod(config, "Gold",    "minecraft:gold_ingot", "minecraft:dye;" + Constants.META_LAPIS, "minecraft:diamond_ore", false,  100,     8, true, false,                null,           0,         0);
+        createDowsingRod(config, "Wood",    "minecraft:stick",      "minecraft:coal",                        "minecraft:iron_ore",    false,   50,     4, true, false,                  "",           0,         0);
+        createDowsingRod(config, "Iron",    "minecraft:iron_ingot", "minecraft:redstone",                    "minecraft:gold_ore",    false,  100,     6, true, false,                  "",           0,         0);
+        createDowsingRod(config, "Gold",    "minecraft:gold_ingot", "minecraft:dye;" + Constants.META_LAPIS, "minecraft:diamond_ore", false,  100,     8, true, false,                  "",           0,         0);
         createDowsingRod(config, "Diamond", "minecraft:diamond",    "minecraft:emerald",                     "",                       true, 1000,     8, true,  true, "minecraft:diamond",           4,        16);
 
         for (DowsingRod rod : dowsingRods) {
